5c6fa024aadfdf0a42b1c23b70fdf493d7fd480f
jgonzalez777 <jorge_gonzalez@cuaed.unam.mx>
2018-07-27 13:58:55 -0500

    Se ha realizado la referencia por archivo de educaedu y google analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,9 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122831332-1"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-122831332-1');
-</script>
